6a9f39110d2932fca4b6a861cc569ca46d2e12c0
amornpan <amornpan@gmail.com>
2022-05-05 13:30:15 +0700

    state_less, state_ful(home.dart), Scaffold, SafeArea

---
+++ lib/screens/home.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: Text("This is home")));
+  }
+}
