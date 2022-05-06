7965a5b9b704603f38f55f7973625d377915e4ff
amornpan <amornpan@gmail.com>
2022-05-06 11:20:18 +0700

    change new image, add fonts, add button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -18,7 +18,44 @@ class _HomeState extends State<Home> {
   }
 
   Widget showAppName() {
-    return Text("Water Flow Estimated");
+    return Text(
+      "ประเมิณอัตราการไหล",
+      style: TextStyle(
+          fontSize: 30.0,
+          color: Colors.lightBlueAccent.shade700,
+          fontFamily: "Kanit"),
+    );
+  }
+
+  Widget signInButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text("เข้าสู่ระบบ"),
+      style:
+          ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 15.0)),
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlinedButton(
+      onPressed: () {},
+      child: Text("สมัครสมาชิก"),
+      style:
+          OutlinedButton.styleFrom(textStyle: const TextStyle(fontSize:  15.0)),
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        signInButton(),
+        SizedBox(
+          width: 10.0,
+        ),
+        signUpButton()
+      ],
+    );
   }
 
   @override
@@ -28,7 +65,17 @@ class _HomeState extends State<Home> {
       child: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: <Widget>[showLogo(), showAppName(), showAppName()],
+          children: <Widget>[
+            showLogo(),
+            SizedBox(
+              height: 8.0,
+            ),
+            showAppName(),
+            SizedBox(
+              height: 8.0,
+            ),
+            showButton()
+          ],
         ),
       ),
     ));
